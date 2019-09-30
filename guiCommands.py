6c03ee2cb59cd624934c8dfed0c48e5cc0dbc91b
Notu <53313361+NotYouDev@users.noreply.github.com>
2019-09-30 10:41:44 -0400

    Update and rename guiCommands to guiCommands.py

---
+++ guiCommands.py
@@ -0,0 +1,11 @@
+import tkinter
+from tkinter import *
+
+def pathgame():
+	game = "Pathfinder"
+	lbl.configure(text = "Pathfinder")
+	print (game)
+def dndgame():
+	game = "DnD5e"
+	lbl.configure(text = "Dungeons and Dragons: 5e")
+	print (game)
