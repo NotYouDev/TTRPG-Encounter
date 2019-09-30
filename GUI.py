b7589aa4c7c67a47d86caff3d7bdd0d99e506033
Notu <53313361+NotYouDev@users.noreply.github.com>
2019-09-30 10:41:04 -0400

    fix syntax, move inline commands to guiCommands

--- GUI.py
+++ GUI.py
@@ -2,7 +2,7 @@
 #import a .py file as commands and such to clean up code
 import tkinter
 from tkinter import *
-
+from guiCommands import pathgame, dndgame
 
 
 window = tkinter.Tk()
@@ -11,19 +11,11 @@ window.geometry('500x500')
 
 lbl = Label(window, text = "Chose Your TTRPG", font=("Arial Bold", 50))
 lbl.grid(column=0, row=0)
-def pathgame():
-	game = "Pathfinder"
-	lbl.configure(text = "Pathfinder")
-	print (game)
-def dndgame():
-	game = "DnD5e"
-	lbl.configure(text = "Dungeons and Dragons: 5e")
-	print (game)
 
-pathfinder = Button (window, bg = "red", text ="Pathfinder", command = pathgame)
-pathfinder.grid(column=1, row=0)
-dnd5e = Button (window, bg = "blue", text ="D&D 5e", command = dndgame)
-dnd5e.grid(column=1, row=0)
+pathfinder = Button (window, bg = "red", text ="Pathfinder", command = pathgame).grid(row=2, column=1)
+dnd5e = Button (window, bg = "blue", text ="D&D 5e", command = dndgame).grid(row=2, column=4)
+
+tk.Button(master, text='Quit', command=master.quit).grid(row=3, column=0, sticky=tk.W, pady=4)
 
 
 pathfinder.pack()
